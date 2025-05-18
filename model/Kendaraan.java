87d38fd70c2e7cdf9e419c8433eaf1497876b6d0
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-18 10:33:11 +0700

    Update Kendaraan.java

--- model/Kendaraan.java
+++ model/Kendaraan.java
@@ -1,40 +1,53 @@
 package model;
 
+import enums.*;
 import interfaces.Showable;
 
 public abstract class Kendaraan implements Showable {
 
-    protected int kapasitas;
-    protected int level;
-    protected int biayaUpgrade;
-    protected int performa;
+    protected KendaraanLevel levelSekarang;
 
-    public Kendaraan(int kapasitas, int level, int biayaUpgrade, int performa) {
-
-        this.kapasitas = kapasitas;
-        this.level = level;
-        this.biayaUpgrade = biayaUpgrade;
-        this.performa = performa;
+    public Kendaraan(KendaraanLevel levelAwal) {
+        this.levelSekarang = levelAwal;
     }
 
     public int getKapasitas() {
-        return kapasitas;
+        return levelSekarang.getKapasitas();
     }
 
     public int getLevel() {
-        return level;
+        return levelSekarang.getLevel();
     }
 
     public int getBiayaUpgrade() {
-        return biayaUpgrade;
+        return levelSekarang.getBiayaUpgrade();
     }
 
     public int getPerforma() {
-        return performa;
+        return levelSekarang.getPerforma();
     }
+
+    public boolean isMaxLevel() {
+        return levelSekarang.getLevel() == KendaraanLevel.LEVEL_15.getLevel();
+    }
+
+   public static Kendaraan buatKendaraan(KendaraanLevel level) {
+    KendaraanType type = level.getType();
+    switch (type) {
+        case GEROBAK:
+            return new Gerobak(level);
+        case MOTOR:
+            return new Motor(level);
+        case PICKUP:
+            return new Pickup(level);
+        default:
+            throw new IllegalArgumentException("Tipe kendaraan tidak dikenali.");
+    }
+}
+
 
     public abstract Kendaraan upgradeLevel();
 
     @Override
     public abstract void tampilkanDetail();
-}
+} 
